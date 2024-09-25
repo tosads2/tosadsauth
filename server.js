61ceef65928e71f04acfacd8ef8fcf2f7f78ee87
tosads2 <tosads157@gmail.com>
2024-09-25 12:13:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+
 const cors = require('cors')
 
 const examRouter = require("./routes/examRoutes.js")
